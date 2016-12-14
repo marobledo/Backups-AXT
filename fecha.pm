ed620e6cfe284424f9bb331a9108ac26f724bd00
marobledo <marioa@ymail.com>
2016-12-14 08:45:41 -0600

    modulo fecha

---
+++ fecha.pm
@@ -0,0 +1,53 @@
+#!/usr/bin/perl
+#use Time::Local;
+use strict;
+use warnings;
+
+sub fecha()
+{
+    my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time());
+    $year+=1900;
+    $mon+=1;
+    if ($mon <= 9) 
+    {
+         $mon="0".$mon;
+    }    
+    if ($mday > 9) 
+    {
+        return $mday."-".$mon."-".$year;
+    }
+    else
+    {
+        return "0".$mday."-".$mon."-".$year;    
+    }
+}
+
+sub fecha_ayer()
+{
+    my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time() - 24 * 60 * 60);
+    $year+=1900;
+    $mon+=1;
+    if ($mon <= 9)
+    {
+         $mon="0".$mon;
+    }
+    if ($mday > 9) 
+    {
+        return $mday."-".$mon."-".$year;
+    }
+    else
+    {
+        return "0".$mday."-".$mon."-".$year;    
+    }
+}
+
+sub hora()
+{
+    my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time());
+    return $hour.":".$min.":".$sec;
+}
+
+
+
+my $fecha = fecha();
+my $fecha_ayer = fecha_ayer();
